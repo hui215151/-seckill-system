e737dd7ad2c6c9548e8a8a883a6733d294d8e59b
曹辉平 <2508128745@qq.com>
2026-07-22 14:34:07 +0800

    Update ScheduleConfig.java

--- src/main/java/com/seckill/config/ScheduleConfig.java
+++ src/main/java/com/seckill/config/ScheduleConfig.java
@@ -1,1 +1,9 @@
+package com.seckill.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
