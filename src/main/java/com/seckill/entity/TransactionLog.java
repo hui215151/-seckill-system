457ae5f528adc90e236cc2b86db348d3f0afdc9b
曹辉平 <2508128745@qq.com>
2026-07-22 14:29:45 +0800

    Update TransactionLog.java

--- src/main/java/com/seckill/entity/TransactionLog.java
+++ src/main/java/com/seckill/entity/TransactionLog.java
@@ -1,1 +1,21 @@
+package com.seckill.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TransactionLog {
+    
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    
+    private String transactionId;
+    private String businessType;
+    private String businessId;
+    private Integer status; // 0-处理中 1-成功 2-失败
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
