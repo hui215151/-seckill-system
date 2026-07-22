d69868c291d4bd5ca01b6fc9cd7ec5de84374a3e
曹辉平 <2508128745@qq.com>
2026-07-22 14:30:58 +0800

    Update TransactionLogMapper.java

--- src/main/java/com/seckill/mapper/TransactionLogMapper.java
+++ src/main/java/com/seckill/mapper/TransactionLogMapper.java
@@ -1,1 +1,9 @@
+package com.seckill.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.seckill.entity.TransactionLog;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TransactionLogMapper extends BaseMapper<TransactionLog> {
+}
