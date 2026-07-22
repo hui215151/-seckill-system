13bbe395413817629e601a62d82e16d5ed12648e
曹辉平 <2508128745@qq.com>
2026-07-22 14:29:20 +0800

    Update SeckillOrder.java

--- src/main/java/com/seckill/entity/SeckillOrder.java
+++ src/main/java/com/seckill/entity/SeckillOrder.java
@@ -1,1 +1,23 @@
+package com.seckill.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class SeckillOrder {
+    
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    
+    private Long userId;
+    private Long goodsId;
+    private String goodsName;
+    private BigDecimal seckillPrice;
+    private Integer status; // 0-创建中 1-已支付 2-已取消
+    private LocalDateTime createTime;
+    private LocalDateTime payTime;
+}
