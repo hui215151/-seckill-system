47f1dfc90652707914502d7842458fe3262c661f
曹辉平 <2508128745@qq.com>
2026-07-22 14:33:20 +0800

    Update GlobalExceptionHandler.java

--- src/main/java/com/seckill/exception/GlobalExceptionHandler.java
+++ src/main/java/com/seckill/exception/GlobalExceptionHandler.java
@@ -1,1 +1,22 @@
+package com.seckill.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public Map<String, Object> handleException(Exception e) {
+        log.error("系统异常：{}", e.getMessage());
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", 500);
+        result.put("message", e.getMessage());
+        return result;
+    }
+}
