f855853c452dbe7d413eb619b342b9ee202cf737
曹辉平 <2508128745@qq.com>
2026-07-22 14:31:28 +0800

    Update SeckillService.java

--- src/main/java/com/seckill/service/SeckillService.java
+++ src/main/java/com/seckill/service/SeckillService.java
@@ -1,1 +1,16 @@
+package com.seckill.service;
 
+import com.seckill.entity.SeckillOrder;
+
+public interface SeckillService {
+    
+    /**
+     * 执行秒杀
+     */
+    SeckillOrder doSeckill(Long userId, Long goodsId);
+    
+    /**
+     * 查询秒杀结果
+     */
+    SeckillOrder getSeckillResult(Long userId, Long goodsId);
+}
