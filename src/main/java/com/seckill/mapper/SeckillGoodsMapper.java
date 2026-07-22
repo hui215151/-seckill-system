6a935ee156c468db88a216750d35f85a9cd0c105
曹辉平 <2508128745@qq.com>
2026-07-22 14:30:13 +0800

    Update SeckillGoodsMapper.java

--- src/main/java/com/seckill/mapper/SeckillGoodsMapper.java
+++ src/main/java/com/seckill/mapper/SeckillGoodsMapper.java
@@ -1,1 +1,16 @@
+package com.seckill.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.seckill.entity.SeckillGoods;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface SeckillGoodsMapper extends BaseMapper<SeckillGoods> {
+    
+    // 乐观锁扣减库存
+    @Update("UPDATE seckill_goods SET stock_count = stock_count - 1, version = version + 1 " +
+            "WHERE id = #{goodsId} AND stock_count > 0 AND version = #{version}")
+    int deductStock(@Param("goodsId") Long goodsId, @Param("version") Integer version);
+}
